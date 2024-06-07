103e0f07d2df7ec42096f02bec6e3b8e754c4554
snguyen98 <snguyen.cs@outlook.com>
2024-06-07 18:14:41 +0100

    Cleaned up code

--- app/views/auth.py
+++ app/views/auth.py
@@ -8,8 +8,9 @@ from ..data.db import get_db
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 def login_required(view):
-    """View decorator that redirects anonymous users to the login page."""
-
+    """
+    View decorator that redirects anonymous users to the login page
+    """
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
@@ -21,8 +22,10 @@ def login_required(view):
 
 @auth_bp.before_app_request
 def load_logged_in_user():
-    """If a user id is stored in the session, load the user object from
-    the database into ``g.user``."""
+    """
+    If a user id is stored in the session, load the user object from
+    the database into g.user
+    """
     user_id = session.get("user_id")
 
     if user_id is None:
@@ -34,7 +37,9 @@ def load_logged_in_user():
 
 @auth_bp.route("/login", methods=("GET", "POST"))
 def login():
-    """Log in a registered user by adding the user id to the session."""
+    """
+    Log in a registered user by adding the user id to the session
+    """
     if request.method == "POST":
         user_id = request.form["user_id"]
         password = request.form["password"]
@@ -62,6 +67,38 @@ def login():
 
 @auth_bp.route("/logout")
 def logout():
-    """Clear the current session, including the stored user id."""
+    """
+    Clear the current session, including the stored user id
+    """
     session.clear()
     return redirect(url_for("auth.login"))
+
+
+@auth_bp.route("/set_password")
+def set_password():
+    """
+    Set user password based on form submission
+    """
+    if request.method == "POST":
+        password = request.form["user_id"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+        user = db.execute(
+            "SELECT * FROM user WHERE userid = ?", (user_id,)
+        ).fetchone()
+
+        if user is None:
+            error = "Incorrect User ID."
+        elif not check_password_hash(user["password"], password):
+            error = "Incorrect password."
+
+        if error is None:
+            # store the user id in a new session and return to the index
+            session.clear()
+            session["user_id"] = user["userid"]
+            return redirect(url_for("index"))
+
+        flash(error)
+
+    return render_template("login.html")
