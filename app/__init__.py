c7de5a9722a33e1550fb824a10057e9ddec94636
snguyen98 <snguyen.cs@outlook.com>
2024-06-07 18:12:12 +0100

    Added booking validation

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
 
 def create_app():
+    import logging.config
+    logging.basicConfig(
+        filename='app/logs/app.log',
+        encoding='utf-8',
+        format='%(asctime)8s     %(levelname)8s     %(message)8s',
+        level=logging.DEBUG
+    )
+    
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = '90a9eee2f650fa61390458ae4b717e14503f7cbe26fabca850b67db78c73319b'
