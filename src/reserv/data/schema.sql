34e745b0d8a809c5fd791d507b517c2724330a1e
snguyen98 <snguyen.cs@outlook.com>
2024-07-14 17:40:23 +0100

    Updated schema to add user status column

--- src/reserv/data/schema.sql
+++ src/reserv/data/schema.sql
@@ -1,18 +1,19 @@
-CREATE TABLE "user" (
+PRAGMA user_version = 1;
+
+CREATE TABLE user (
 	"userid"	TEXT NOT NULL UNIQUE,
 	"displayname"	TEXT UNIQUE,
 	"password"	TEXT,
+	"status"	TEXT NOT NULL DEFAULT "active" CHECK(
+		status = "active" OR
+		status = "inactive" OR
+		status = "terminated"
+	),
 	PRIMARY KEY("userid")
 );
 
-CREATE TABLE "schedule" (
+CREATE TABLE schedule (
 	"date"	TEXT NOT NULL CHECK(date("date") IS NOT NULL) UNIQUE,
 	"userid"	TEXT NOT NULL,
-	PRIMARY KEY("date")
-);
-
-CREATE TABLE "archive" (
-	"date"	TEXT NOT NULL CHECK(date("date") IS NOT NULL) UNIQUE,
-	"userid"	TEXT,
 	PRIMARY KEY("date")
 );
