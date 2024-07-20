f4bafe9f8cd5463f3e7ee9938e70776b73ca4f22
snguyen98 <snguyen.cs@outlook.com>
2024-07-20 19:18:14 +0100

    Added check for view permission on schedule page

--- src/reserv/views/schedule.py
+++ src/reserv/views/schedule.py
@@ -3,6 +3,7 @@ from datetime import date, timedelta
 import logging
 
 from .auth import login_required_view
+from ..data.query import get_user_permissions, get_perm_by_name
 
 schedule_bp = Blueprint("schedule", __name__)
 
@@ -12,14 +13,20 @@ def index():
     """
     Defines g.schedule to be used in the schedule template (index.html)
     """
-    g.today = date.today()
+    perms = get_user_permissions(g.user["user_id"])
+    
+    if get_perm_by_name("view") in perms:
+        g.today = date.today()
 
-    # Calculates the date of the monday of the current week
-    week_start = g.today + timedelta(days=-g.today.weekday(), weeks=0)
+        # Calculates the date of the monday of the current week
+        week_start = g.today + timedelta(days=-g.today.weekday(), weeks=0)
 
-    # Generates a list of dates from the week start for the next 14 days
-    g.schedule = [week_start + timedelta(days=i) for i in range(14)]
+        # Generates a list of dates from the week start for the next 14 days
+        g.schedule = [week_start + timedelta(days=i) for i in range(14)]
 
-    logging.debug(f"Setting schedule for w/c {week_start}")
+        logging.debug(f"Setting schedule for w/c {week_start}")
 
-    return render_template('index.html')
+        return render_template('index.html')
+    
+    else:
+        return render_template('access_denied.html')
