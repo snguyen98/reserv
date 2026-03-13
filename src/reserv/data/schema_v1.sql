8a0fbd93f5a0cdaa819490de01fd5372e0a23d84
snguyen98 <snguyen.cs@outlook.com>
2026-03-13 22:59:38 +0000

    Added availability table to SQL

--- src/reserv/data/schema_v1.sql
+++ src/reserv/data/schema_v1.sql
@@ -34,6 +34,23 @@ CREATE TABLE schedule (
 	PRIMARY KEY("date")
 );
 
+CREATE TABLE availability (
+    "date"	TEXT NOT NULL CHECK(date("date") IS NOT NULL) UNIQUE,
+    "created_on" TEXT NOT NULL CHECK(date("created_on")) DEFAULT (
+        strftime('%Y-%m-%d %H:%M:%S', 'now')
+    ),
+    "updated_on" TEXT NOT NULL CHECK(date("updated_on")) DEFAULT (
+        strftime('%Y-%m-%d %H:%M:%S', 'now')
+    ),
+	"user_id" TEXT NOT NULL,
+	"status" TEXT NOT NULL DEFAULT "unavailable" CHECK(
+        status = "available" OR
+        status = "unavailable"
+    ),
+	PRIMARY KEY("date", "user_id")
+);
+
+
 CREATE TABLE role (
 	"id"	INTEGER NOT NULL UNIQUE,
 	"name"	TEXT,
